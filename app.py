c7ab1ed6b6b5aee6c1893121e2624c4746df9f1d
unknown <paragbuch7@gmail.com>
2023-07-12 16:05:19 +0530

    first commit

--- app.py
+++ app.py
@@ -1,39 +1,42 @@
 import pickle
-from flask import Flask,request,app,jsonify,url_for,render_template
+from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
-model=pickle.load(open('model.pkl','rb'))
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+# model = pickle.load(open('Bean_model.pickle', 'rb'))
 @app.route('/')
 def home():
-    #return 'Hello World'
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-
-    data=request.json['data']
-    print(data)
-    new_data=[list(data.values())]
-    output=model.predict(new_data)[0]
-    return jsonify(output)
-
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST','GET'])
 def predict():
+    if request.method == 'POST':
+        try:
+            Area=int(request.form['Area'])
+            MajorAxisLength=float(request.form['MajorAxisLength'])
+            MinorAxisLength=float(request.form['MinorAxisLength'])
+            Eccentricity=float(request.form['Eccentricity'])
+            Extent=float(request.form['Extent'])
+            Solidity=float(request.form['Solidity'])
+            roundness=float(request.form['roundness'])
+            ShapeFactor2=float(request.form['ShapeFactor2'])
+            ShapeFactor4=float(request.form['ShapeFactor4'])
+            filename = 'Bean_model.pickle'
+            loaded_model = pickle.load(open(filename, 'rb')) # loading the model file from the storage
+            # predictions using the loaded model file
+            prediction=loaded_model.predict([[Area,MajorAxisLength, MinorAxisLength, Eccentricity, Extent, Solidity, roundness,ShapeFactor2,ShapeFactor4]])
+            print('prediction is', prediction)
+            # showing the prediction results in a UI
+            return render_template('home.html',prediction=prediction)
 
-    data=[float(x) for x in request.form.values()]
-    final_features = [np.array(data)]
-    print(data)
-    
-    output=model.predict(final_features)[0]
-    print(output)
-    #output = round(prediction[0], 2)
-    return render_template('home.html', prediction_text="Airfoil pressure is  {}".format(output))
-
-
-
-if __name__=="__main__":
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+    # return render_template('results.html')
+    else:
+        return render_template('index.html')
+if __name__ == "__main__":
     app.run(debug=True)
-
-
